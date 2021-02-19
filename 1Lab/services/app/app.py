7e0a90c01fda4501fe5c05267b8026c7d2e4721a
Moxxiq <dimanavsisto@gmail.com>
2021-02-19 17:52:34 +0200

    Init. Lab1 docker-compose run

---
+++ 1Lab/services/app/app.py
@@ -0,0 +1,23 @@
+import psycopg2
+import os
+
+
+def list_files(startpath):
+    # copy-pastle
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        yield '{}{}/'.format(indent, os.path.basename(root))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            yield '{}{}'.format(subindent, f)
+
+
+def main():
+    print("I'm working")
+    with open('struct.txt', 'w') as f:
+        f.write("\n".join(list_files('/data/')))
+
+
+if __name__ == '__main__':
+    main()
