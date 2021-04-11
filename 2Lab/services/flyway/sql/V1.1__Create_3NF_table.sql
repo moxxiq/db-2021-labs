1066b6014db8f3ce53f6f353720b1561951c0161
Moxxiq <dimanavsisto@gmail.com>
2021-04-11 19:14:08 +0300

    New 3NF structure

--- 2Lab/services/flyway/sql/V1.1__Create_3NF_table.sql
+++ 2Lab/services/flyway/sql/V1.1__Create_3NF_table.sql
@@ -1,4 +1,4 @@
-drop table if exists participant, place, eduinst, test, participant_place, eduinst, place_type cascade;
+drop table if exists participant, place, eduplace, test, eo;
 drop type if exists lang, teststatus cascade;
 
 CREATE TYPE lang AS ENUM (
@@ -7,68 +7,56 @@ CREATE TYPE lang AS ENUM (
 CREATE TYPE teststatus AS ENUM (
     'Зараховано', 'Анульовано', 'Не подолав поріг', 'Не з’явився');
 
-CREATE TABLE participant
-(
-    outid            character varying(36) NOT NULL
-        constraint participant_pkey primary key,
-    birth            smallint              NOT NULL,
-    sextypename      sex                   NOT NULL,
-    regtypename      text                  NOT NULL,
-    classprofilename text,
-    classlangname    lang,
-    year             smallint              NOT NULL
-);
-
 CREATE TABLE place
 (
+    placeid  serial primary key,
     regname  text,
     areaname text,
     tername  text,
-    placeid  serial
-        constraint place_pkey primary key,
     unique (regname, areaname, tername)
 );
 
-CREATE TABLE eduinst
+CREATE TABLE eo
 (
-    eoname        text
-        constraint eduinst_pkey primary key,
-    eotypename    text,
-    eoparent      text,
-    place_placeid serial
-        references place (placeid)
+    eoid    serial primary key,
+    placeid serial references place (placeid),
+    eoname  text,
+    unique (placeid, eoname)
+);
+
+CREATE TABLE eduplace
+(
+    eoid        serial references eo (eoid) primary key,
+    eotypename  text,
+    eoparent    text,
+    tertypename ter
+);
+
+CREATE TABLE participant
+(
+    outid            character varying(36) primary key,
+    placeid          serial references place (placeid),
+    eduplaceid       serial references eduplace (eoid),
+    birth            smallint NOT NULL,
+    sextypename      sex      NOT NULL,
+    regtypename      text     NOT NULL,
+    classprofilename text,
+    classlangname    lang,
+    year             smallint NOT NULL
 );
 
 CREATE TABLE test
 (
-    subjectname       text,
-    status            teststatus,
-    ball100           numeric,
-    ball12            numeric,
-    ball              numeric,
-    adaptscale        integer,
-    langname          lang,
-    testid            serial
-        constraint test_pkey primary key,
-    participant_outid character varying(36) NOT NULL
-        references participant (outid),
-    place_placeid     serial
-        references place (placeid),
-    unique (subjectname, participant_outid)
-);
-
-CREATE TABLE participant_place
-(
-    participant_outid character varying(36) NOT NULL
-        references participant (outid),
-    place_placeid     serial
-        references place (placeid),
-    unique (participant_outid, place_placeid)
-);
-
-CREATE TABLE place_type
-(
-    place_placeid serial unique
-        references place (placeid),
-    tertypename   ter
+    testid     serial primary key,
+    outid      character varying(36) NOT NULL references participant (outid),
+    eoid       serial references eo (eoid),
+    name       text,
+    status     teststatus,
+    ball100    numeric,
+    ball12     numeric,
+    ball       numeric,
+    adaptscale integer,
+    langname   lang,
+    dpalevel   text,
+    unique (outid, name)
 );
--- 2Lab/services/flyway/sql/V1.2__Move_data_otable_3NF.sql
+++ 2Lab/services/flyway/sql/V1.2__Move_data_otable_3NF.sql
@@ -1,213 +1,270 @@
-insert into place (regname, areaname, tername)
-select distinct regname, areaname, tername
-from odata
-union
-select ukrptregname,
-       regexp_replace(ukrptareaname, '. ' || ukrpttername || '$', ''),
-       ukrpttername
-from odata
-union
-select histptregname,
-       regexp_replace(histptareaname, '. ' || histpttername || '$', ''),
-       histpttername
-from odata
-union
-select mathptregname,
-       regexp_replace(mathptareaname, '. ' || mathpttername || '$', ''),
-       mathpttername
-from odata
-union
-select physptregname,
-       regexp_replace(physptareaname, '. ' || physpttername || '$', ''),
-       physpttername
-from odata
-union
-select chemptregname,
-       regexp_replace(chemptareaname, '. ' || chempttername || '$', ''),
-       chempttername
-from odata
-union
-select bioptregname,
-       regexp_replace(bioptareaname, '. ' || biopttername || '$', ''),
-       biopttername
-from odata
-union
-select geoptregname,
-       regexp_replace(geoptareaname, '. ' || geopttername || '$', ''),
-       geopttername
-from odata
-union
-select engptregname,
-       regexp_replace(engptareaname, '. ' || engpttername || '$', ''),
-       engpttername
-from odata
-union
-select fraptregname,
-       regexp_replace(fraptareaname, '. ' || frapttername || '$', ''),
-       frapttername
-from odata
-union
-select deuptregname,
-       regexp_replace(deuptareaname, '. ' || deupttername || '$', ''),
-       deupttername
-from odata
-union
-select spaptregname,
-       regexp_replace(spaptareaname, '. ' || spapttername || '$', ''),
-       spapttername
-from odata
-union
-select eoregname,
-       regexp_replace(eoareaname, '. ' || eotername || '$', ''),
-       eotername
-from odata
-on conflict do nothing;
+with odata as (
+    select *
+    from public.odata
+    limit 1000
+),
+--      with
+         fixed_areaname_subjects as (
+    select distinct outid,
+                    ukrptregname                                                   as regname,
+                    regexp_replace(ukrptareaname, '. ' || ukrpttername || '$', '') as areaname,
+                    ukrpttername                                                   as tername,
+                    ukrptname                                                      as ptname,
+                    ukrtest                                                        as name,
+                    ukrteststatus                                                  as status,
+                    ukrball100                                                     as ball100,
+                    ukrball12                                                      as ball12,
+                    ukrball                                                        as ball,
+                    ukradaptscale                                                  as adaptscale,
+                    NULL                                                           as langname,
+                    NULL                                                           as dpalevel
+    from odata
+    union
+    select outid,
+           histptregname,
+           regexp_replace(histptareaname, '. ' || histpttername || '$', ''),
+           histpttername,
+           histptname,
+           histtest       as name,
+           histteststatus as status,
+           histball100    as ball100,
+           histball12     as ball12,
+           histball       as ball,
+           NULL,
+           histlang       as langname,
+           NULL           as dpalevel
+    from odata
+    union
+    select outid,
+           mathptregname,
+           regexp_replace(mathptareaname, '. ' || mathpttername || '$', ''),
+           mathpttername,
+           mathptname,
+           mathtest       as name,
+           mathteststatus as status,
+           mathball100    as ball100,
+           mathball12     as ball12,
+           mathball       as ball,
+           NULL,
+           mathlang       as langname,
+           NULL           as dpalevel
+    from odata
+    union
+    select outid,
+           physptregname,
+           regexp_replace(physptareaname, '. ' || physpttername || '$', ''),
+           physpttername,
+           physptname,
+           phystest       as name,
+           physteststatus as status,
+           physball100    as ball100,
+           physball12     as ball12,
+           physball       as ball,
+           NULL,
+           physlang       as langname,
+           NULL           as dpalevel
 
-delete
-from place
-where tername is null
-   or areaname is null
-   or regname is null;
+    from odata
+    union
+    select outid,
+           chemptregname,
+           regexp_replace(chemptareaname, '. ' || chempttername || '$', ''),
+           chempttername,
+           chemptname,
+           chemtest       as name,
+           chemteststatus as status,
+           chemball100    as ball100,
+           chemball12     as ball12,
+           chemball       as ball,
+           NULL,
+           chemlang       as langname,
+           NULL           as dpalevel
 
-insert into place_type (place_placeid, tertypename)
-select distinct place.placeid, odata.tertypename
-from odata
-         join place
-              on odata.regname = place.regname
-                  and odata.areaname = place.areaname
-                  and odata.tername = place.tername
-on conflict do nothing ;
+    from odata
+    union
+    select outid,
+           bioptregname,
+           regexp_replace(bioptareaname, '. ' || biopttername || '$', ''),
+           biopttername,
+           bioptname,
+           biotest       as name,
+           bioteststatus as status,
+           bioball100    as ball100,
+           bioball12     as ball12,
+           bioball       as ball,
+           NULL,
+           biolang       as langname,
+           NULL          as dpalevel
+    from odata
+    union
+    select outid,
+           geoptregname,
+           regexp_replace(geoptareaname, '. ' || geopttername || '$', ''),
+           geopttername,
+           geoptname,
+           geotest       as name,
+           geoteststatus as status,
+           geoball100    as ball100,
+           geoball12     as ball12,
+           geoball       as ball,
+           NULL,
+           geolang       as langname,
+           NULL          as dpalevel
+    from odata
+    union
+    select outid,
+           engptregname,
+           regexp_replace(engptareaname, '. ' || engpttername || '$', ''),
+           engpttername,
+           engptname,
+           engtest       as name,
+           engteststatus as status,
+           engball100    as ball100,
+           engball12     as ball12,
+           engball       as ball,
+           NULL,
+           NULL,
+           engdpalevel   as dpalevel
 
-insert into participant(outid, birth, sextypename, regtypename, classprofilename, classlangname, year)
-select distinct outid, birth, sextypename, regtypename, classprofilename, classlangname::lang, year
-from odata
-on conflict do nothing;
-
-insert into participant_place (participant_outid, place_placeid)
-select distinct odata.outid, place.placeid
-from odata
-         join place
-              on odata.regname = place.regname
-                  and odata.areaname = place.areaname
-                  and odata.tername = place.tername
-on conflict do nothing ;
-
-insert into eduinst(eoname, eotypename, eoparent, place_placeid)
-select distinct odata.eoname, odata.eotypename, odata.eoparent, place.placeid
-from odata
-         join place
-              on odata.regname = place.regname
-                  and odata.areaname = place.areaname
-                  and odata.tername = place.tername
-where eoname is not NULL
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, adaptscale, participant_outid, place_placeid)
-select ukrtest, ukrteststatus::teststatus, ukrball100, ukrball12, ukrball, ukradaptscale, odata.outid, place.placeid
-from odata
-         join place
-              on odata.ukrptregname = place.regname
-                  and odata.ukrptareaname = place.areaname
-                  and odata.ukrpttername = place.tername
-where ukrtest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, langname, participant_outid, place_placeid)
-select histtest, histteststatus::teststatus, histball100, histball12, histball, histlang::lang, odata.outid, place.placeid
-from odata
-         join place
-              on odata.histptregname = place.regname
-                  and odata.histptareaname = place.areaname
-                  and odata.histpttername = place.tername
-where histtest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, langname, participant_outid, place_placeid)
-select mathtest, mathteststatus::teststatus, mathball100, mathball12, mathball, mathlang::lang, odata.outid, place.placeid
-from odata
-         join place
-              on odata.mathptregname = place.regname
-                  and odata.mathptareaname = place.areaname
-                  and odata.mathpttername = place.tername
-where mathtest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, langname, participant_outid, place_placeid)
-select phystest, physteststatus::teststatus, physball100, physball12, physball, physlang::lang, odata.outid, place.placeid
-from odata
-         join place
-              on odata.physptregname = place.regname
-                  and odata.physptareaname = place.areaname
-                  and odata.physpttername = place.tername
-where phystest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, langname, participant_outid, place_placeid)
-select chemtest, chemteststatus::teststatus, chemball100, chemball12, chemball, chemlang::lang, odata.outid, place.placeid
-from odata
-         join place
-              on odata.chemptregname = place.regname
-                  and odata.chemptareaname = place.areaname
-                  and odata.chempttername = place.tername
-where chemtest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, langname, participant_outid, place_placeid)
-select biotest, bioteststatus::teststatus, bioball100, bioball12, bioball, biolang::lang, odata.outid, place.placeid
-from odata
-         join place
-              on odata.bioptregname = place.regname
-                  and odata.bioptareaname = place.areaname
-                  and odata.biopttername = place.tername
-where biotest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, langname, participant_outid, place_placeid)
-select geotest, geoteststatus::teststatus, geoball100, geoball12, geoball, geolang::lang, odata.outid, place.placeid
-from odata
-         join place
-              on odata.geoptregname = place.regname
-                  and odata.geoptareaname = place.areaname
-                  and odata.geopttername = place.tername
-where geotest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, participant_outid, place_placeid)
-select engtest, engteststatus::teststatus, engball100, engball12, engball, odata.outid, place.placeid
-from odata
-         join place
-              on odata.engptregname = place.regname
-                  and odata.engptareaname = place.areaname
-                  and odata.engpttername = place.tername
-where engtest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, participant_outid, place_placeid)
-select fratest, frateststatus::teststatus, fraball100, fraball12, fraball, odata.outid, place.placeid
-from odata
-         join place
-              on odata.fraptregname = place.regname
-                  and odata.fraptareaname = place.areaname
-                  and odata.frapttername = place.tername
-where fratest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, participant_outid, place_placeid)
-select deutest, deuteststatus::teststatus, deuball100, deuball12, deuball, odata.outid, place.placeid
-from odata
-         join place
-              on odata.deuptregname = place.regname
-                  and odata.deuptareaname = place.areaname
-                  and odata.deupttername = place.tername
-where deutest is not null
-on conflict do nothing;
-
-insert into test(subjectname, status, ball100, ball12, ball, participant_outid, place_placeid)
-select spatest, spateststatus::teststatus, spaball100, spaball12, spaball, odata.outid, place.placeid
-from odata
-         join place
-              on odata.spaptregname = place.regname
-                  and odata.spaptareaname = place.areaname
-                  and odata.spapttername = place.tername
-where spatest is not null
-on conflict do nothing;
+    from odata
+    union
+    select outid,
+           fraptregname,
+           regexp_replace(fraptareaname, '. ' || frapttername || '$', ''),
+           frapttername,
+           fraptname,
+           fratest       as name,
+           frateststatus as status,
+           fraball100    as ball100,
+           fraball12     as ball12,
+           fraball       as ball,
+           NULL,
+           NULL,
+           fradpalevel   as dpalevel
+    from odata
+    union
+    select outid,
+           deuptregname,
+           regexp_replace(deuptareaname, '. ' || deupttername || '$', ''),
+           deupttername,
+           deuptname,
+           deutest       as name,
+           deuteststatus as status,
+           deuball100    as ball100,
+           deuball12     as ball12,
+           deuball       as ball,
+           NULL,
+           NULL,
+           deudpalevel   as dpalevel
+    from odata
+    union
+    select outid,
+           spaptregname,
+           regexp_replace(spaptareaname, '. ' || spapttername || '$', ''),
+           spapttername,
+           spaptname,
+           spatest       as name,
+           spateststatus as status,
+           spaball100    as ball100,
+           spaball12     as ball12,
+           spaball       as ball,
+           NULL,
+           NULL,
+           spadpalevel   as dpalevel
+    from odata),
+     fixed_area_eo as (
+         select distinct outid,
+                         eoregname                                                as regname,
+                         regexp_replace(eoareaname, '. ' || eotername || '$', '') as areaname,
+                         eotername                                                as tername,
+                         eoname,
+                         eotypename,
+                         eoparent,
+                         tertypename
+         from odata
+     ),
+     place_inserted as (
+         insert into place (regname, areaname, tername)
+             select distinct regname, areaname, tername
+             from odata
+             union
+             select distinct regname, areaname, tername
+             from fixed_areaname_subjects
+             union
+             select distinct regname, areaname, tername
+             from fixed_area_eo
+--              where regname is not null
+             returning placeid, regname, areaname, tername
+     ),
+     eo_inserted as (
+         insert into eo (placeid, eoname)
+             select distinct placeid, eoname
+             from (select distinct regname, areaname, tername, eoname
+                   from fixed_area_eo
+                   union
+                   select regname, areaname, tername, ptname
+                   from fixed_areaname_subjects) as eos
+                      join place_inserted on
+                     eos.tername = place_inserted.tername
+                     and eos.areaname = place_inserted.areaname
+                     and eos.tername = place_inserted.tername
+             returning eoid, placeid, eoname
+     ),
+     eduplace_inserted as (
+         insert into eduplace (eoid, eotypename, eoparent, tertypename)
+             select distinct eo_inserted.eoid, eotypename, eoparent, tertypename::ter
+             from fixed_area_eo
+                      join place on fixed_area_eo.regname = place.regname
+                 and fixed_area_eo.areaname = place.areaname
+                 and fixed_area_eo.tername = place.tername
+                      join eo_inserted on eo_inserted.eoname = fixed_area_eo.eoname
+                 and eo_inserted.placeid = place.placeid
+             returning eoid
+     ),
+     participant_inserted as (
+         insert into participant (outid, placeid, eduplaceid, birth, sextypename, regtypename, classprofilename,
+                                  classlangname, year)
+             select odata.outid,
+                    regplace.placeid,
+                    eo_inserted.eoid,
+                    odata.birth,
+                    odata.sextypename::sex,
+                    odata.regtypename,
+                    odata.classprofilename,
+                    odata.classlangname::lang,
+                    odata.year
+             from odata
+                     join place_inserted regplace on
+                     odata.regname = regplace.regname
+                     and odata.areaname = regplace.areaname
+                     and odata.tername = regplace.areaname
+                      left join fixed_area_eo
+                                on odata.outid = fixed_area_eo.outid
+                      left join place_inserted
+                                on fixed_area_eo.regname = place_inserted.regname
+                                    and fixed_area_eo.areaname = place_inserted.areaname
+                                    and fixed_area_eo.tername = place_inserted.tername
+                      left join eo_inserted
+                                on eo_inserted.placeid = place_inserted.placeid
+                                    and eo_inserted.eoname = fixed_area_eo.eoname
+             returning outid, placeid, eduplaceid
+     )
+insert
+into test (outid, eoid, name, status, ball100, ball12, ball, adaptscale, langname, dpalevel)
+select outid,
+       eo_inserted.eoid,
+       name,
+       fsub.status::teststatus,
+       ball100,
+       ball12,
+       ball,
+       adaptscale,
+       langname::lang,
+       dpalevel
+from fixed_areaname_subjects as fsub
+         join place_inserted pins on fsub.regname = pins.regname
+    and fsub.areaname = pins.areaname
+    and fsub.tername = pins.tername
+         join eo_inserted on eo_inserted.placeid = pins.placeid
+    and fsub.ptname = eo_inserted.eoname
+where fsub.name is not null;
